c343f1ec088150a5132bb29d1ae516fc543021e6
spyrex69 <snaved159@gmail.com>
2025-04-20 13:11:14 +0530

    falling enemy update

--- Assets/Scripts/FallingEnemy.cs
+++ Assets/Scripts/FallingEnemy.cs
@@ -3,23 +3,6 @@ using UnityEngine;
 
 public class FallingEnemy : MonoBehaviour
 {
-    [SerializeField] private float fallSpeed = 5f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateEnemy();
-    }
-
-    private void UpdateEnemy()
-    {
-        transform.Translate(Vector2.down * fallSpeed * Time.deltaTime);
-    }
 
     private void LateUpdate()
     {
